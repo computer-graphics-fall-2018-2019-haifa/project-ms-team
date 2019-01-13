d956c38d4d86b7e8e08d1e52804871c7c2837a5c
makolath <makolath@hotmail.com>
2019-01-13 19:28:00 +0200

    Textures with Lighting model

--- Viewer/shaders/fshader.glsl
+++ Viewer/shaders/fshader.glsl
@@ -42,10 +42,23 @@ out vec4 frag_color;
 
 void main()
 {
+	vec4 AmbientColor = material.AmbientColor;
+	vec4 DiffuseColor = material.DiffuseColor;
+	vec4 SpecualrColor = material.SpecualrColor;
+
+	//texturing if any
+	if (hasTex) {
+		vec3 textureColor = vec3(texture(material.textureMap, fragTexCoords));
+		AmbientColor = vec4(textureColor, 1.0f);
+		DiffuseColor = vec4(textureColor, 1.0f);
+		SpecualrColor = vec4(textureColor, 1.0f);
+		//frag_color = vec4(textureColor, 1.0f);
+	}
+
 	vec3 N = normalize(fragNormal.xyz / fragNormal.w);
 	vec3 V = normalize(fragPos.xyz / fragPos.w);	//assuming camera is always at 0,0,0
 	// ambient is only needed once
-	vec4 ac = material.KA * material.AmbientColor;
+	vec4 ac = material.KA * AmbientColor;
 	vec4 IA = clamp(vec4(ac.x * SceneAmbient.x, ac.y * SceneAmbient.y, ac.z * SceneAmbient.z, 1.0f), 0.0f, 1.0f);
 
 	vec4 ID = vec4(0.0f);
@@ -59,25 +72,16 @@ void main()
 		vec3 R = normalize(reflect(-L, N));
 
 		float LN = max(dot(N, L), 0.0f);
-		vec4 dc = material.KD * LN * material.DiffuseColor;
+		vec4 dc = material.KD * LN * DiffuseColor;
 		ID = ID + clamp(vec4(dc.x * lightColor.x, dc.y * lightColor.y, dc.z * lightColor.z, 1.0f), 0.0f, 1.0f);
 
 		float RV = pow(max(dot(R, V), 0.0f), material.KSE);
-		vec4 sc = material.KS * RV * material.SpecualrColor;
+		vec4 sc = material.KS * RV * SpecualrColor;
 		IS = IS + clamp(vec4(sc.x * lightColor.x, sc.y * lightColor.y, sc.z * lightColor.z, 1.0f), 0.0f, 1.0f);
 
 	}
 
 	frag_color = clamp(IA + ID + IS, 0.0f, 1.0f);
-
-	//texturing if any
-	if (hasTex) {
-		vec3 textureColor = vec3(texture(material.textureMap, fragTexCoords));
-		vec4 AmbientColor = vec4(textureColor, 1.0f);
-		vec4 DiffuseColor = vec4(textureColor, 1.0f);
-		vec4 SpecualrColor = vec4(textureColor, 1.0f);
-		frag_color = vec4(textureColor, 1.0f);
-	}
 
 	// fog coloring
 	float dist = abs(fragPos.z / fragPos.w);
